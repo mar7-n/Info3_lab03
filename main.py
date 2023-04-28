e6cb7e076693d89064de9a6b30518deaa791698a
anhphuc.hoang <phuchoangpkr89@gmail.com>
2023-04-28 17:21:06 +0200

    add logic part

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import math
+
+
 def onlyAlphabetCharacters(txt):
     for i in range(len(txt)):
         letter = ord(txt[i])
         if not (letter>64 and letter<91) and not (letter>96 and letter<123):
             return False
     return True
+
 
 def main():
 
@@ -16,19 +20,106 @@ def main():
         "If you want to close the program, please type 'exit corrector'.\n"
         "Now type your word please and press enter: ")
 
-    while(txt != "exit corrector"):
+    while txt != "exit corrector":
         txt = txt.replace(' ', '')
-        while(not onlyAlphabetCharacters(txt)):
+        while not onlyAlphabetCharacters(txt):
             txt = input("That's wrong. You can just use letters from the english alphabet.\n"
                   "Please insert a new word and press enter: ")
             txt = txt.replace(' ', '')
+
         txt = txt.upper()
 
         # PHUC PART
 
-        txt = input("Please insert a new word and press enter: ")
+        if search_word_in_file(txt):
+            txt = input("This word is correct! But I'm still hungry. Gimme another word, please: ")
+        else:
+            print("This word is incorrect. Maybe your word is one in the list below: ")
+            print(find_recommended_word(txt))
+            txt = input("Please insert a new word and press enter: ")
 
     print("Thanks for using our programm. Have a nice day!")
+
+
+def search_word_in_file(word):
+    file = open("generated_words.txt", "r")
+    lines = file.readlines()
+    num_lines = len(lines)
+
+    top = num_lines
+    bottom = 0
+    pointer = math.floor((top + bottom) / 2)
+    while word[0] != lines[pointer][0]:
+        if word[0] > lines[pointer][0]:
+            bottom = pointer
+        if word[0] < lines[pointer][0]:
+            top = pointer
+        pointer = math.floor((top + bottom) / 2)
+
+    direction = 1
+    for index in range(1, len(word)-1):
+        if word[index] > lines[pointer][index]:
+            break
+        if word[index] < lines[pointer][index]:
+            direction = -1
+            break
+
+    while word[0] == lines[pointer][0]:
+        if word + "\n" == lines[pointer]:
+            return True
+        else:
+            pointer += direction
+
+    return False
+
+
+def find_recommended_word(input_word):
+    file = open("generated_words.txt", "r")
+    recommended_list = dict()
+
+    for line in file:
+        possibly_right_word = line.strip()
+
+        check_grade = calculate_grade(input_word, possibly_right_word)
+
+        if check_grade >= 75:
+            recommended_list[possibly_right_word] = check_grade
+
+    recommended_words = dict(sorted(recommended_list.items(), key=lambda x: x[1], reverse=True))
+
+    return list(recommended_words)[:20]
+
+def calculate_grade(input_word, possibly_right_word):
+    check_grade = 100 - 8 * abs(len(input_word) - len(possibly_right_word))
+
+    mark = 0
+    unmatched_count = 0
+    for index, letter in enumerate(possibly_right_word):
+        if not mark < len(input_word):
+            break
+        if letter == input_word[mark]:
+            mark += 1
+            continue
+        elif mark + 1 < len(input_word) and index + 1 < len(possibly_right_word) \
+                and letter == input_word[mark + 1] and possibly_right_word[index + 1] == input_word[mark]:
+            mark += 2
+            index += 2
+            check_grade -= 5
+            continue
+        else:
+            is_found_in_the_rest = False
+            for i in range(mark, len(input_word)):
+                if input_word[i] == letter:
+                    unmatched_count += (i - mark)
+                    mark = i + 1
+                    break
+            if not is_found_in_the_rest:
+                unmatched_count += 1
+
+        check_grade -= unmatched_count * 5
+
+    return check_grade
+
 
 if __name__ == "__main__":
     main()
